f82a9351d49d0421f467dbf620444f61ee8f1029
LDerka <derkal15@gmail.com>
2023-06-13 10:40:39 -0300

    init PI Rick and Morty

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import Cards from './components/Cards.jsx';
+import Nav from './components/Nav';
+import { useState } from 'react';
+import axios from 'axios';
+
+
+
+function App() {
+
+   const [characters, setCharacters] = useState([]);
+
+   const onSearch = (id) => {
+      axios(`https://rickandmortyapi.com/api/character/${id}`)
+      .then(response => response.data)
+      .then((data) => {
+         if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+
+   const onClose = (id) => {
+      const charactersFiltered = characters.filter(character => character.id !== Number(id))
+      setCharacters(charactersFiltered);
+   }
+
+   return (
+      <div className='App'>
+         <Nav onSearch={onSearch}/>
+         <Cards characters={characters} onClose={onClose}/>
+      </div>
+   );
+}
+
+export default App;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const SearchBar = ({onSearch}) => {
+   
+   const [id, setId] = useState('');
+
+   const handleChange = (event) => {
+      setId(event.target.value)
+   }
+
+   
+   return(
+      <div>
+         {
+            <>
+            <input type="search" onChange={handleChange} value={id}></input>
+            <button onClick={() => {onSearch(id); setId('')}}>Agregar</button>
+            </>
+         }
+      </div>
+   );
+}
+
+export default SearchBar; 
